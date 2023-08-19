d1ec070a3bc1dcc9b62ef39e2176f8db6439e58b
Mungunerdene <Mungunerdeneblog@gmail.com>
2023-08-19 14:40:13 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,68 @@
-import './App.css';
-import { useState } from 'react';
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from "react";
+import * as Components from './Components';
 
 
 
 function App() {
-  const namesFromDatabase = [
-    {id: 1, username: 'Naraa'},
-    {id: 2, username: 'Bat'},
-    {id: 3, username: 'Saraa'},
-    {id: 4, username: 'Oyun'},
-    {id: 5, username: 'Hulan'},
-    {id: 6, username: 'Erdene'},
-    {id: 7, username: 'Jargal'},
-    {id: 8, username: 'Anu'},
-    {id: 9, username: 'Ganbold'},
-    {id: 10, username: 'Herlen'},
-    {id: 11, username: 'Nomin'},
-    {id: 12, username: 'Shuree'},
-    {id: 13, username: 'Togtoh'},
-    {id: 14, username: 'Saihanaa'},
-  ]  
 
-  const [names, setNames] = useState(namesFromDatabase)
+    // component 1
+    const [signIn, toggle] = React.useState(true);
+     return(
+         <Components.Container>
+             <Components.SignUpContainer signinIn={signIn}>
+                 <Components.Form>
+                     <Components.Title>Create Account</Components.Title>
+                     <Components.Input type='text' placeholder='Name' />
+                     <Components.Input type='email' placeholder='Username' />
+                     <Components.Input type='password' placeholder='Password' />
+                     <Components.Button>Sign Up</Components.Button>
+                 </Components.Form>
+             </Components.SignUpContainer>
 
-  const filerNames = e => {
-    const search = e.target.value.toLowerCase()
-    const filteredNames = namesFromDatabase.filter(names => names.username.toLocaleLowerCase().includes(search))
-    setNames(filteredNames)
-  
-  }
+             <Components.SignInContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Sign in</Components.Title>
+                      <Components.Input type='email' placeholder='Username' />
+                      <Components.Input type='password' placeholder='Password' />
+                      <Components.Anchor href='#'>Forgot your 
+                      password?</Components.Anchor>
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Username</h2>
-        <hr/>
-        <input type="text" onChange={(e) => filerNames(e)} />
-        <button>Search</button>
-        <ul>
-          {names.map(name => {
-            return <li key={name.id}> {name.username}</li>
-          })}
-        </ul>
-      </header>
+                      <Components.Anchor className="Rem" href='#'>Remember Me</Components.Anchor>
 
-    </div>
-  )
-  
+
+                      <Components.Button>Sigin In</Components.Button>
+                  </Components.Form>
+             </Components.SignInContainer>
+
+             <Components.OverlayContainer signinIn={signIn}>
+                 <Components.Overlay signinIn={signIn}>
+
+                 <Components.LeftOverlayPanel signinIn={signIn}>
+                     <Components.Title>Welcome Back!</Components.Title>
+                     <Components.Paragraph>
+                         To keep connected with us please login with your personal info
+                     </Components.Paragraph>
+                     <Components.GhostButton onClick={() => toggle(true)}>
+                         Sign In
+                     </Components.GhostButton>
+                     </Components.LeftOverlayPanel>
+
+                     <Components.RightOverlayPanel signinIn={signIn}>
+                       <Components.Title>Welcome to login</Components.Title>
+                       <Components.Paragraph>
+                           Don't have an account?
+                       </Components.Paragraph>
+                           <Components.GhostButton onClick={() => toggle(false)}>
+                               Sigin Up
+                           </Components.GhostButton> 
+                     </Components.RightOverlayPanel>
+ 
+                 </Components.Overlay>
+             </Components.OverlayContainer>
+
+         </Components.Container>
+     )
 }
 
 export default App;
